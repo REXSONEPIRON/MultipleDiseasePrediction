04939c4eb4bdfb5e1860327ca9f5123c7a7722d9
Rexson Epiron <102672443+REXSONEPIRON@users.noreply.github.com>
2025-04-18 23:45:35 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -163,17 +163,17 @@ if selected == 'Home':
 
   with col3:
     st.markdown('<div class="hover-box">', unsafe_allow_html=True)
-    st.image(diabetes_img, width=240)
+    st.image(diabetes_img, width=200)
     st.markdown('<div class="caption">Diabetes Prediction</div></div>', unsafe_allow_html=True)
 
   with col2:
     st.markdown('<div class="hover-box">', unsafe_allow_html=True)
-    st.image(heart_img, width=240)
+    st.image(heart_img, width=200)
     st.markdown('<div class="caption">Cardiovascular Disease prediction</div></div>', unsafe_allow_html=True)
 
   with col1:
     st.markdown('<div class="hover-box">', unsafe_allow_html=True)
-    st.image(lung_img, width=240)
+    st.image(lung_img, width=200)
     st.markdown('<div class="caption">Lung Cancer Prediction</div></div>', unsafe_allow_html=True)
     
           # Inject custom CSS for background and text styling
